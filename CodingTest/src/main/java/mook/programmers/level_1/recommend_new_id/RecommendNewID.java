572134595eec2473277ad8b6f13ad0596932e600
imjeong <imjeong@gplusit.co.kr>
2021-04-12 17:45:20 +0900

    Write the new project problem

---
+++ CodingTest/src/main/java/mook/programmers/level_1/recommend_new_id/RecommendNewID.java
@@ -0,0 +1,45 @@
+package mook.programmers.level_1.recommend_new_id;
+
+public class RecommendNewID {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	// 대문자 -> 소문자
+	public static String step1(String id) {
+		return id.toLowerCase();
+	}
+	
+	// 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 나머지 문자 제거
+	public static String step2(String id) {
+		return "";
+	}
+	
+	// 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환
+	public static String step3(String id) {
+		return "";
+	}
+	
+	// 마침표(.)가 처음이나 끝에 위치하면 제거
+	public static String step4(String id) {
+		return "";
+	}
+	
+	// 빈 문자열이면 "a" 대입
+	public static String step5(String id) {
+		return "";
+	}
+	
+	// 길이 16자 이상이면 첫 15개의 문자를 제외한 나머지 문자 제거
+	// 제거한 후 끝이 마침표(.)면 마침표 제거
+	public static String step6(String id) {
+		return "";
+	}
+	
+	// 길이가 2 이하면 마지막 문자를 길이가 3이 될 때까지 반복해서 붙이기
+	public static String step7(String id) {
+		return "";
+	}
+}
