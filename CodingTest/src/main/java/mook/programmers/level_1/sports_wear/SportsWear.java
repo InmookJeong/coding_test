a4296a5c6910c1f2e5598876d1cedf42ce696dab
imjeong <imjeong@gplusit.co.kr>
2021-04-14 17:30:48 +0900

    Add sports wear project

---
+++ CodingTest/src/main/java/mook/programmers/level_1/sports_wear/SportsWear.java
@@ -0,0 +1,17 @@
+package mook.programmers.level_1.sports_wear;
+
+public class SportsWear {
+
+	public static void main(String[] args) {
+		
+		int n1 = 5; int[] lost1 =  {2, 4}; int[] reserve1 = {1,3,5};
+		int n2 = 5; int[] lost2 =  {2, 4}; int[] reserve2 = {1,3,5};
+		int n3 = 3; int[] lost3 =  {3}; int[] reserve3 = {1};
+		
+	}
+	
+	public static int solution(int n, int[] lost, int[] reserve) {
+		int answer = 0;
+		return answer;
+	}
+}
