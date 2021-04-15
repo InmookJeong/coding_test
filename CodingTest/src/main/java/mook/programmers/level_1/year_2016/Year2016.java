d5ad4f33712f71c7b5bea2f9626f7ea68d19222d
imjeong <imjeong@gplusit.co.kr>
2021-04-15 18:02:04 +0900

    Add year 2016 project

---
+++ CodingTest/src/main/java/mook/programmers/level_1/year_2016/Year2016.java
@@ -0,0 +1,20 @@
+package mook.programmers.level_1.year_2016;
+
+/**
+ * Programmers Coding Test Level 1<br/>
+ * 2016년
+ * 
+ * @author Dev.Mook
+ * @since 2021.04.15
+ */
+public class Year2016 {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public static String solution(int a, int b) {
+		String answer = "";
+		return answer;
+	}
+}
