d44d7bed9fe424d022392c109cd02c77f024866a
imjeong <imjeong@gplusit.co.kr>
2021-04-23 16:59:07 +0900

    add making prime number project

---
+++ CodingTest/src/main/java/mook/programmers/level_1/making_prime_number/MakingPrimeNumber.java
@@ -0,0 +1,20 @@
+package mook.programmers.level_1.making_prime_number;
+
+/**
+ * Programmers Coding Test Level 1<br/>
+ * 소수 만들기 - Summer/Winter Coding(~2018)
+ * 
+ * @author Dev.Mook
+ * @since 2021.04.23
+ */
+public class MakingPrimeNumber {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public int solution(int[] nums) {
+		int answer = -1;
+		return answer;
+	}
+}
